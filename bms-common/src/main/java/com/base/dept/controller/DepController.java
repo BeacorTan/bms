a79c27c1d4f02de9d3d3ed81d4be0efd48dc7f78
beacor <2429516993@qq.com>
2018-05-24 14:56:25 +0800

    调整部门管理

--- bms-common/src/main/java/com/base/dept/controller/DepController.java
+++ bms-common/src/main/java/com/base/dept/controller/DepController.java
@@ -9,7 +9,6 @@ import com.common.framework.util.ModelUtil;
 import com.common.framework.util.ResponseJson;
 import com.common.framework.util.ServiceUtil;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,18 +24,12 @@ import javax.annotation.Resource;
 @RequestMapping(value = "dep")
 public class DepController {
 
+
     @Resource
     private DepService depService;
 
-    @RequestMapping(value = "/update/view", method = RequestMethod.GET)
-    public ModelAndView depUpdatView(String depId, ModelMap modelMap) throws Exception {
-        modelMap.addAttribute("depInfo", depService.selectByPrimaryKey(depId));
-        ModelAndView modelAndView = new ModelAndView("system/dep/dep_update", modelMap);
-        return modelAndView;
 
-    }
-
-    @RequestMapping(value = "", method = RequestMethod.POST)
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
     public ResponseJson addDep(Department department) throws Exception {
         ModelUtil.insertInit(department);
         Department resultDep = depService.insertSelective(department);
@@ -47,7 +40,7 @@ public class DepController {
         return ServiceUtil.getResponseJson("部门添加成功", true, jsonObject);
     }
 
-    @RequestMapping(value = "", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/del", method = RequestMethod.DELETE)
     public ResponseJson deleteDep(Department department) throws Exception {
         department.setActiveFlag(BaseModel.ACTIVE_FLAG_NO);//状态改为-1,代表逻辑删除
         ModelUtil.updateInit(department);
@@ -55,34 +48,6 @@ public class DepController {
         return ServiceUtil.getResponseJson("删除成功", true, null);
     }
 
-    @RequestMapping(value = "", method = RequestMethod.PUT)
-    public ResponseJson updateDep(Department department) throws Exception {
-        ModelUtil.updateInit(department);
-        Department resultDep = depService.updateByPrimaryKeySelective(department);
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("id", resultDep.getId());
-        jsonObject.put("pId", resultDep.getParentId());
-        jsonObject.put("name", resultDep.getDepName());
-        return ServiceUtil.getResponseJson("部门修改成功", true, jsonObject);
-    }
-
-    @RequestMapping(value = "info/{id}")
-    public ModelAndView depInfo(@PathVariable(value = "id") String id, ModelMap modelMap) throws Exception {
-        modelMap.addAttribute("depInfo", depService.selectByPrimaryKey(id));
-        ModelAndView modelAndView = new ModelAndView("system/dep/dep_info", modelMap);
-        return modelAndView;
-    }
-
-    @RequestMapping(value = "/main", method = RequestMethod.GET)
-    public ModelAndView depMain() {
-        return new ModelAndView("system/dep/dep_main");
-    }
-
-    @RequestMapping(value = "/add/view", method = RequestMethod.GET)
-    public ModelAndView addView(String depId, ModelMap modelMap) {
-        modelMap.addAttribute("parentId", depId);
-        return new ModelAndView("system/dep/dep_add");
-    }
 
     @RequestMapping(value = "/json/list", method = RequestMethod.GET)
     public ResponseJson depJsonData(String roleCode) throws Exception {
@@ -90,10 +55,15 @@ public class DepController {
     }
 
 
-    @RequestMapping(value = "/alert/list", method = RequestMethod.GET)
-    public ModelAndView alterList() throws Exception {
-        return new ModelAndView("system/dep/dep_list");
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public ModelAndView depMain() {
+        return new ModelAndView("system/dep/dept_main");
     }
 
+    @RequestMapping(value = "/profile", method = RequestMethod.GET)
+    public ModelAndView addView(String depId, ModelMap modelMap) {
+        modelMap.addAttribute("parentId", depId);
+        return new ModelAndView("system/dep/dept_profile");
+    }
 
 }
